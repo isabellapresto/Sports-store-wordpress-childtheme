a064ea65872e1dbb5dc7e8350cad01cf2f011d19
isabellapresto <isabella.presto@gmail.com>
2023-04-26 14:18:30 +0200

    custom post type added

--- functions.php
+++ functions.php
@@ -16,6 +16,42 @@ add_action( 'wp_head', 'change_header_background_color' );
 
 //tar bort produktbeskrivning
 function remove_product_description() {
-	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 }
 add_action( 'woocommerce_single_product_summary', 'remove_product_description', 1 );
+
+function store_post_types () {
+
+$args = array(
+
+'supports' => array('title', 'editor', 'thumbnail'),
+
+'menu_icon' => 'dashicons-store',
+
+'rewrite' => array('slug' => 'stores'),
+
+'has_archive' => true,
+
+'public' => true,
+
+'labels' => array(
+
+'name' => 'Stores',
+
+'add_new_item' => 'Add New Campus',
+
+'edit_item' => 'Edit Store',
+
+'all_items' => 'All Stores',
+
+'singular_name' => 'Store'
+
+ )
+
+ );
+
+	register_post_type('stores', $args);
+
+}
+
+add_action('init', 'store_post_types');
